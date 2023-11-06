16d60303053d0cb6e3ab12f5491c9e9dd345aafb
Ahmed <ahmed.zammel.info@gmail.com>
2023-11-06 11:01:22 +0100

    Add PostDto.

--- src/main/java/com/blog/payload/PostDto.java
+++ src/main/java/com/blog/payload/PostDto.java
@@ -1,4 +1,14 @@
 package com.blog.payload;
 
+import lombok.Data;
+
+@Data
 public class PostDto {
+
+    //TODO: DTO should be a record.
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+
 }
