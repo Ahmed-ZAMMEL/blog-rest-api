821b9689f0d25efa0635725e5358f269c54bd71e
Ahmed <ahmed.zammel.info@gmail.com>
2023-11-11 10:59:02 +0100

    Add Post entity.

--- src/main/java/com/blog/entity/Post.java
+++ src/main/java/com/blog/entity/Post.java
@@ -2,6 +2,7 @@ package com.blog.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 
+@Builder
 @Entity
 @Table(
         name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
